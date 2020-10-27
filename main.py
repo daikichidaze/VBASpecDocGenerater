7d653a2b19bd96cf5e199fe2c5d9c67dfaa46fad
takuma-yoshioka-tt <yoshioka@ttni.co.th>
2020-10-27 19:52:48 +0700

    Modify for properties

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import json
 import pandas as pd
 from glob import glob
 from os import path
+from more_itertools import flatten
 
 import execjs
 
@@ -29,21 +30,32 @@ def create_json_from_vba(vba_code_path: str) -> dict:
     result_str = ctx.call(const.docgen_func_name, vba_text)
     return json.loads(result_str)
 
+# The vba script order must be module -> property
 def change_json_format(json_data: dict) -> dict:
-    module_list = json_data['Module']['Module']
+    if len(json_data['Module']) == 0:
+        raise ValueError('No Module/Property discription')
+
+    module_list = json_data['Module'].get('Module', [])
+    property_list  = json_data['Module'].get('Property', [])
+
+    total_pairs = [('Module', x) for x in module_list] + [('Property', x) for x in property_list]
+
     procedure_list = json_data['Procedures']
     output = []
 
-    num_of_modules = len(module_list)
+    num_of_items = len(total_pairs)
+
+    if num_of_items < len(procedure_list):
+        raise ValueError('No Module discription is not enough')
     
-    for i in range(num_of_modules):
-        output += create_one_module_output(module_list[i], procedure_list[i], i)
+    for i in range(num_of_items):
+        output += create_one_module_output(total_pairs[i], procedure_list[i], i)
     return json.dumps(output)
 
-def create_one_module_output(module_str: str, procedure_dict: dict, step_idx: int) -> list:
+def create_one_module_output(module_pair: tuple, procedure_dict: dict, step_idx: int) -> list:
     tmp_output = []
-    tmp_output.append({'h1' : 'Module: ' + procedure_dict['Name']})
-    tmp_output.append({'h2': module_str})
+    tmp_output.append({'h1' : module_pair[0] + ': ' + procedure_dict['Name']}) # todo: change to acsept property
+    tmp_output.append({'h2': module_pair[1]})
 
     tmp_output.append({'h3': 'Basic infomation'})
     table_dict = set_default_table_dict(['key', 'value'])
@@ -52,10 +64,20 @@ def create_one_module_output(module_str: str, procedure_dict: dict, step_idx: in
     table_dict['table']['rows'] = df_tmp.values.tolist()
     tmp_output.append(table_dict)
 
+    if 'Pro' in module_pair[0]:
+        print()
+
     if not procedure_dict['Param'] is None:
         tmp_output.append({'h3': 'Parameter'})
-        df_tmp = pd.DataFrame(procedure_dict['Param'], 
-                            index = [f'Param{i+1}' for i in range(len(procedure_dict['Param']))]).reset_index(drop= False)
+
+        if isinstance(procedure_dict['Param'], list): # Case of several parameter
+            df_tmp = pd.DataFrame(procedure_dict['Param'], 
+                                index = [f'Param{i+1}' for i in range(len(procedure_dict['Param']))]).reset_index(drop= False)
+        elif isinstance(procedure_dict['Param'],dict): # Case of one parameter
+            df_tmp = pd.Series(procedure_dict['Param'], name = 'Param1').to_frame().T.reset_index(drop=False)
+        else:
+            raise ValueError('Unknown json data')
+
         df_tmp = df_tmp.astype(str)
         table_dict = set_default_table_dict(df_tmp.columns.to_list()) 
         table_dict['table']['rows'] = df_tmp.to_dict('records')
@@ -68,7 +90,6 @@ def set_default_table_dict(header_names: list) -> dict:
 
 def convert_json_to_md(json_data : dict):
     doc_data_json = change_json_format(json_data)
-
 
     with open(const.convert_code_path) as js_file:
         js_text = js_file.read()
@@ -88,13 +109,12 @@ if __name__ == "__main__":
     vba_file_name_lists = glob(path.join(args.directory, f'**.{const.vba_module_ext}')) + glob(path.join(args.directory, f'**.{const.vba_class_ext}'))
 
     for path in vba_file_name_lists:
-        if 'Button' in path:
-            result_json = create_json_from_vba('xdocgen/test.bas')
-            result_md = convert_json_to_md(result_json)
+        result_json = create_json_from_vba('xdocgen/test.bas')#(path)
+        result_md = convert_json_to_md(result_json)
     
-    with open('test.md', mode='w') as f:
-        f.write(result_md)
-            
+        with open('test.md', mode='w') as f:
+            f.write(result_md)
+        break
 
 
 
